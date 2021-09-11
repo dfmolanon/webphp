399dd990e12dfb4bedaf5f90ca74e49d3aad4bd1
dfmolanon <75173364+dfmolanon@users.noreply.github.com>
2021-09-11 17:01:10 -0500

    Add files via upload

---
+++ productos.php
@@ -0,0 +1,30 @@
+<?php include("template/cabecera.php")?>
+
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/20210508_210838-COLLAGE.jpg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/IMG_20210712_110613830.jpg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>    
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="card">
+        <img class="card-img-top" src="./img/EFFECTS.jpg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
+    </div>
+</div>
+<?php include("template/pie.php")?>
